7eea3a4ec35fe005b44604ada1952b13480e14ae
abhisarmittal <abhisarmittal1@gmail.com>
2019-11-09 12:37:49 -0500

    Moved onClickListener to sell from mainActivity

--- app/src/main/java/org/mort11/marketplaceapp/Sell.java
+++ app/src/main/java/org/mort11/marketplaceapp/Sell.java
@@ -28,15 +28,15 @@ public class Sell extends Fragment {
         submit.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
-                TextInputEditText productNameField = view.findViewById(R.id.product_name);
-                TextInputEditText productPriceField = view.findViewById(R.id.product_price);
-                TextInputEditText productDescriptionField = view.findViewById(R.id.product_description);
+                //TextInputEditText productNameField = view.findViewById(R.id.product_name);
+                //TextInputEditText productPriceField = view.findViewById(R.id.product_price);
+                //TextInputEditText productDescriptionField = view.findViewById(R.id.product_description);
 
-                String productName = productNameField.getText().toString();
-                double productPrice = Double.parseDouble(productPriceField.getText().toString());
-                String productDescription = productDescriptionField.getText().toString();
+                //String productName = productNameField.getText().toString();
+                //double productPrice = Double.parseDouble(productPriceField.getText().toString());
+                //String productDescription = productDescriptionField.getText().toString();
 
-                Product userProduct = new Product(productName, productPrice, productDescription);
+                //Product userProduct = new Product(productName, productPrice, productDescription);
 
             }
         });
