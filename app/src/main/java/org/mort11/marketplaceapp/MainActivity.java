9b790ad03efada2c138e46f9228b05134ba329da
abhisarmittal <abhisarmittal1@gmail.com>
2019-11-09 12:21:45 -0500

    Changed text to hint for prod name

--- app/src/main/java/org/mort11/marketplaceapp/MainActivity.java
+++ app/src/main/java/org/mort11/marketplaceapp/MainActivity.java
@@ -6,11 +6,14 @@ import androidx.viewpager.widget.ViewPager;
 import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 
 import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.textfield.TextInputEditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
 
     private static Context context;
     @Override
@@ -19,6 +22,23 @@ public class MainActivity extends AppCompatActivity {
         context = getApplicationContext();
         setContentView(R.layout.activity_main);
         initTabs();
+
+        Button submit = findViewById(R.id.submitButton);
+        submit.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                TextInputEditText productNameField = findViewById(R.id.product_name);
+                TextInputEditText productPriceField = findViewById(R.id.product_price);
+                TextInputEditText productDescriptionField = findViewById(R.id.product_description);
+
+                String productName = productNameField.getText().toString();
+                double productPrice = Double.parseDouble(productPriceField.getText().toString());
+                String productDescription = productDescriptionField.getText().toString();
+
+                Product userProduct = new Product(productName, productPrice, productDescription);
+                
+            }
+        });
     }
 
     public void initTabs() {
@@ -31,5 +51,7 @@ public class MainActivity extends AppCompatActivity {
         viewPager.setAdapter(tabAdapter);
         tabs.setupWithViewPager(viewPager);
     }
+
+
 
 }
