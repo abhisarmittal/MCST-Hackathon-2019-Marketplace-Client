d65da002936338e855cb1371f0c18f87360dd135
abhisarmittal <abhisarmittal1@gmail.com>
2019-11-09 12:33:04 -0500

    Moved onClickListener to sell from mainActivity

--- app/src/main/java/org/mort11/marketplaceapp/MainActivity.java
+++ app/src/main/java/org/mort11/marketplaceapp/MainActivity.java
@@ -6,12 +6,8 @@ import androidx.viewpager.widget.ViewPager;
 import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
-import android.view.WindowManager;
-import android.widget.Button;
 
 import com.google.android.material.tabs.TabLayout;
-import com.google.android.material.textfield.TextInputEditText;
 
 public class MainActivity extends AppCompatActivity{
 
@@ -22,23 +18,6 @@ public class MainActivity extends AppCompatActivity{
         context = getApplicationContext();
         setContentView(R.layout.activity_main);
         initTabs();
-
-        Button submit = findViewById(R.id.submitButton);
-        submit.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view) {
-                TextInputEditText productNameField = findViewById(R.id.product_name);
-                TextInputEditText productPriceField = findViewById(R.id.product_price);
-                TextInputEditText productDescriptionField = findViewById(R.id.product_description);
-
-                String productName = productNameField.getText().toString();
-                double productPrice = Double.parseDouble(productPriceField.getText().toString());
-                String productDescription = productDescriptionField.getText().toString();
-
-                Product userProduct = new Product(productName, productPrice, productDescription);
-                
-            }
-        });
     }
 
     public void initTabs() {
--- app/src/main/java/org/mort11/marketplaceapp/Sell.java
+++ app/src/main/java/org/mort11/marketplaceapp/Sell.java
@@ -1,9 +1,16 @@
 package org.mort11.marketplaceapp;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.google.android.material.textfield.TextInputEditText;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -15,6 +22,24 @@ public class Sell extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.selling_tab, container, false);
+
+        Button submit = view.findViewById(R.id.submitButton);
+        Log.d("Custom", "" + submit);
+        submit.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                TextInputEditText productNameField = view.findViewById(R.id.product_name);
+                TextInputEditText productPriceField = view.findViewById(R.id.product_price);
+                TextInputEditText productDescriptionField = view.findViewById(R.id.product_description);
+
+                String productName = productNameField.getText().toString();
+                double productPrice = Double.parseDouble(productPriceField.getText().toString());
+                String productDescription = productDescriptionField.getText().toString();
+
+                Product userProduct = new Product(productName, productPrice, productDescription);
+
+            }
+        });
         return view;
     }
 
