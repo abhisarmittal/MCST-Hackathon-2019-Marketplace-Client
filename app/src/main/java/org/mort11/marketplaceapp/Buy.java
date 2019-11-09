84dab6c5bccaf01057d994ddd94a15010071e865
Vedh Koutha <vkoutha@gmail.com>
2019-11-09 11:24:13 -0500

    Linked tabs to fragments

---
+++ app/src/main/java/org/mort11/marketplaceapp/Buy.java
@@ -0,0 +1,21 @@
+package org.mort11.marketplaceapp;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class Buy extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.buying_tab, container, false);
+        return view;
+    }
+
+}
